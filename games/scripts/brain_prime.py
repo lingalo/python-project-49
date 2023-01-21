df5c9a2bdb306830053793e6d571fa3373295b14
lingalo <bokova.nast@yandex.ru>
2023-01-21 21:32:35 +0300

    new brain_prime

---
+++ games/scripts/brain_prime.py
@@ -0,0 +1,18 @@
+from games.logic import logic
+
+
+start = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(num1, num2 = None):
+    print(f'Question: {num1}')
+    if num1 == 1:
+        return 'no'
+    for x in range(2, num1):
+        if num1 % x == 0:
+            return 'no'
+    return 'yes'
+
+
+if __name__ == '__main__':
+    logic(start, is_prime)
