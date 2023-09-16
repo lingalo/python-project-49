aea076db65175d9993a5732394ecde3ab9ce3a23
lingalo <bokova.nast@yandex.ru>
2023-09-16 16:25:23 +0300

    Refactoring logic.py

--- brain_games/logic.py
+++ brain_games/logic.py
@@ -1,6 +1,9 @@
 import prompt
 
 
+WRONG_ANSWER = ' is wrong answer ;(. Correct answer was '
+
+
 def logic(RULES, game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
@@ -8,15 +11,14 @@ def logic(RULES, game):
     print(RULES)
     count = 0
     for x in range(0, 3):
-        correct_answer, number = game()
-        print(f'Question: {number}')
-        print(f'читерство: {correct_answer}')
+        correct_answer, question = game()
+        print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
         if answer == str(correct_answer):
             print('Correct!')
             count += 1
         else:
-            print(f'\'{answer}\' is wrong answer ;(. Correct answer was \'{correct_answer}\')')
+            print(f"'{answer}'{WRONG_ANSWER}'{correct_answer}'")
             print(f'Let\'s try again {name}!')
             break
     if count == 3:
