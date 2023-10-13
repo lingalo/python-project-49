84e339e62edaa056124051ed213b3351b4b96a0d
lingalo <bokova.nast@yandex.ru>
2023-10-13 23:26:13 +0300

    Refactoring the gcd function

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,11 +5,8 @@ def gcd():
     divider = randint(2, 10)
     num1 = randint(2, 10) * divider
     num2 = randint(2, 10) * divider
-    correct_answer = divider
-    for x in range(divider, min(num1, num2) + 1):
+    question = f'{num1} {num2}'
+    for x in range(min(num1, num2), divider, -1):
         if not num1 % x and not num2 % x:
-            if x > divider:
-                correct_answer = x
-        x += 1
-    numbers = f'{num1} {num2}'
-    return (correct_answer, numbers)
+            return (x, question)
+    return (divider, question)
